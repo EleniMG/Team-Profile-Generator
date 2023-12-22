7380116a1fa7f60dd748a1a9c1350072aeddaeb7
Eleni <elenimariex@gmail.com>
2023-12-22 21:31:00 +0000

    CHanging inquirer version and creating questions for each employee

--- starter/index.js
+++ starter/index.js
@@ -23,5 +23,79 @@ const questions = [
         type: 'number',
         name: 'managerID',
         message: "Please enter the manager's ID"
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: "Please enter the manager's email"
+    },
+    {
+        type: 'number',
+        name: 'officeNumber',
+        message: "Please enter the office number"
     }
 ]
+
+const newEmployee = {
+    type: 'list',
+    name: 'nextEmployee',
+    message: "Please select an option:",
+    choices: ["Engineer", "Intern", "Finished building my team"]
+}
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: "Please enter the engineer's name"
+    },
+    {
+        type: 'number',
+        name: 'engineerID',
+        message: "Please enter the engineer's ID"
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: "Please enter the engineer's email"
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please enter the GitHub username"
+    }
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: "Please enter the intern's name"
+    },
+    {
+        type: 'number',
+        name: 'internID',
+        message: "Please enter the intern's ID"
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: "Please enter the intern's email"
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "Please enter the intern's school name"
+    }
+]
+
+
+// function to initialize program
+const init = () => inquirer.prompt(questions);
+
+init()
+    .then((questions) => {
+        inquirer.prompt(newEmployee);
+        console.log(questions)
+        console.log(newEmployee.name)})
+
