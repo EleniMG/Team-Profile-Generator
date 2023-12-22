1dd895a4f3f86e7c3169a9f464ea0123af43b887
Eleni <elenimariex@gmail.com>
2023-12-22 00:34:07 +0000

    Creating Intern subclass of Employee

--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee.js");
+
+class Intern extends Employee{
+    constructor(school){
+        super(name, id, email);
+        this.school = school;
+    }
+
+    getSchool(){
+        console.log(`School: ${this.school}`)
+    }
+
+    getRole(){
+        console.log("Role: Intern")
+    }
+
+}
+
+module.exports = Intern;
